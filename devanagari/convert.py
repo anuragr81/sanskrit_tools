8ef69856f2caafd0d1235e148dea08d2b9175b03
Anurag S <anurag.srivastava@riskcare.com>
2022-12-25 15:36:19 +0000

    Adding devanagari output.

--- devanagari/convert.py
+++ devanagari/convert.py
@@ -1,20 +1,45 @@
 import re
 
-def hal():
-    return ('kha',"kh",'ka',"k",'gha',"gh",'ga',"g",'Nga',"Ng",'Nca',"Nc","NN",'Nna',"Nn",\
-            'chha',"chh",'cha',"ch",'jha',"jh",'ja',"j",
-            'Xtha',"Xth",'Xta',"Xt",'Xdha',"Xdh",'Xda',"Xd",'Xsha',"Xsh",\
-            'tha',"th",'ta',"t",'dha',"dh",'da',"d",'na',"n",\
-            'pha',"ph",'pa',"p",'bha',"bh",'ba',"b",'ma',"m",'ya',"y",'ra',"r",\
-            'la',"l",'va',"v",'sha',"sh",
-            'sa',"s",'ha',"h")
 
-def parse_string(input_str):
+"""
+Matras sorted by length of string to parse
+"""
+def matras():
+    return  ( ('Rii',"\u0944"), ('Ri',"\u0943"),  ('au',"\u094C"),('ai',"\u0948"),\
+        ('ii',"\u0940"),('aa',"\u093E"),('uu',"\u0942"),('i',"\u093F"), ('u',"\u0941"),\
+         ('e',"\u0947"), ('o',"\u094B"), )
+
+"""
+matra-combined-hals sorted by length of string to parse
+"""
+def hals_to_combine():
+    return tuple([('kh',True),('k',True),('gh',True),('g',True),('Ng',True),\
+        ('Nc',True),('NN',False),('Nn',True) ,( 'chh',True ), ('ch',True),\
+        ('jh',True),('j',True), ('Xth',True), ('Xt',True), ('Xdh',True),\
+        ('Xd',True), ('Xsh',True), ('th',True), ('t',True), ('dh',True),\
+        ('d',True), ('n',True), ('ph',True), ('p',True), ('bh',True),('b',True),\
+        ('m',True), ('y',True), ('r',True),('l',True), ('v',True), ('sh',True),\
+        ('s',True), ('h',True) ])
+
+
+"""
+Combine hals with matras
+"""
+def hals_combined():
+    mts = [k for k,_ in matras()]+['a']
+    combined_list =[]
+    for ch, togen in hals_to_combine():
+        if togen:
+            combined_list = combined_list  + [ ch+x for x in mts] +[ch]
+            
+    return combined_list 
+
+def parse_string_for_devanagari(input_str):
     """
     build a list of aksharas from the string - unknown letters are ignored
     """
-    sorted_achs= ('lRii','Rii', 'lRi', 'Ri','ai', "ii","uu",'au',"aa",'a', 'i', 'u', 'e', 'o',)
-    match_re = "("+'|'.join(hal() + sorted_achs)+")" + "(.*)"
+    sorted_achs= ('lRii','Rii', 'lRi', 'Ri','ai', "ii","uu",'au',"aa",'a', 'i', 'u', 'e', 'o','M')
+    match_re = "("+'|'.join(tuple(hals_combined()) + sorted_achs)+")" + "(.*)"
 
 
     matches = True
@@ -34,8 +59,9 @@ def parse_string(input_str):
 Devanagari map taken from  https://everythingfonts.com/unicode/devanagari
 """
 def devanagari_map():
-    return {
+    mainmap =  {
             'NN': "\u0901",
+            'M': "\u0902",
             'a': "\u0905",
             'aa': "\u0906",
             'i': "\u0907",
@@ -58,6 +84,8 @@ def devanagari_map():
             'gh': "\u0918\u094D",
             'Nga': "\u0919",
             'Ng': "\u0919\u094D",
+            'chha':"\u091B",
+            'chh':"\u091B\u094D",
             'cha': "\u091A",
             'ch': "\u091A\u094D",
             'ja': "\u091C",
@@ -96,15 +124,44 @@ def devanagari_map():
             'bh': "\u092D\u094D",
             'ma': "\u092E",
             'm': "\u092E\u094D",
-            'M': "\u0902",
-            'M': "\u0902",
-            'M': "\u0902",
-            'M': "\u0902",
-            'M': "\u0902",
-            'M': "\u0902",
-            'M': "\u0902",
-            'M': "\u0902",
-            'M': "\u0902"}
+            'ya': "\u092F",
+            'y': "\u092F\u094D",
+            'ra': "\u0930",
+            'r': "\u0930\u094D",
+            'la': "\u0932",
+            'l': "\u0932\u094D",
+            'va': "\u0935",
+            'v': "\u0935\u094D",
+            'sha': "\u0936",
+            'sh': "\u0937\u094D",
+            'Xsha': "\u0937",
+            'Xsh': "\u0937\u094D",
+            'sa': "\u0938",
+            's': "\u0938\u094D",
+            'ha': "\u0939",
+            'h': "\u0939\u094D",
+            'S': "\u093D",
+            }
 
-print(parse_string("aaka"))
+
+    for lhs,togen in hals_to_combine():
+        # only take first element onf mainmap[lhs] because the second element is halant
+        if togen:
+            entry = [(lhs+k,mainmap[lhs][0]+v) for k,v in matras() ]
+            #print (entry)
+            mainmap.update(dict(entry))
+
+    return mainmap
+
+
+def convert_to_devanagari(strParse):
+    strparsed = parse_string_for_devanagari(strParse)
+    m = devanagari_map()
+    return ''.join(m[ch] for ch in strparsed)   
+    
+
+
+
+def test():
+    print(convert_to_devanagari('laabhaM labhyate'))
 
