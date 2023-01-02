ccce11987bbe47df0d27a62bf3492d5261a03bfc
Anurag S <anandmukh@yahoo.com>
2023-01-02 15:55:44 +0000

    Adding ascii in request.

--- panini/pratyaya.py
+++ panini/pratyaya.py
@@ -7,7 +7,7 @@ from sanskrit_tools.panini.devanagari.convert import convert_to_devanagari
 output in devanagari using mapping from https://everythingfonts.com/unicode/devanagari
 """
 def get_all_suffixes():
-    return json.dumps(dict( ( convert_to_devanagari(x) , '') for x in all_pratyayas()))
+    return json.dumps(dict( ( convert_to_devanagari(x) , {'ascii':x}) for x in all_pratyayas()))
 
 def is_suptingant(pratyaya):
     if pratyaya in sup_pratyayaaH() or pratyaya in tiNg_pratyayaaH():
