8a76e54ce66dc27b04b47a9d4eb8226190260ec8
Anurag S <anandmukh@yahoo.com>
2023-06-17 13:39:56 +0000

    Added expression builder for reducer.

--- panini/expressiontree.py
+++ panini/expressiontree.py
@@ -2,6 +2,7 @@
 import json
 
 from panini.sutras.common_definitions import Dhaatu,Node,Suffix, parse_string
+from panini.dhaatus import dhaatus_meaning
 
 from generate_path import *
 
@@ -9,5 +10,32 @@ from panini.devanagari.convert import parse_devanagari_to_ascii
 
 from pprint import pprint
 
+"""
+Node structure contains additional information such as the lakaara as well. lakaara can be selected after
+a tibaadi suffix is selected. subaadi can also be simplified in this manner.
+"""
+def prepare_node_structure(arr):
+    all_dhaatus = [ ''.join(dhaatus_meaning()[k]['ascii']) for k in dhaatus_meaning().keys()]
+    tibaadi_suffixes = ('tip','tas','jhi','sip','thas','tha','mip','vas','mas','ta','aataam','jha','thaa','sa','aathaam','dhvam','iXt','vahi','mahiNg',)
+    ep=[]
+    if len(arr)>1:
+       if arr[0] in  all_dhaatus:
+           ep.append(Node(Dhaatu(parse_string(arr[0])),parent1=None))
+           for x in arr[1:]:
+               if x in tibaadi_suffixes:
+                   #TODO: consider inputs other than laXt
+                   ep.append(Node(Suffix(x,lakaara='laXt'),parent1=None))
+           return ep
+
+    return []
+
+
+"""
+  returns the expression in form of a cytoscape graph
+"""
 def get_expression_tree(expression):
-    return json.dumps([ ''.join(parse_devanagari_to_ascii(x)) for x in expression.split(",")])
+    arrInp=[ ''.join(parse_devanagari_to_ascii(x)) for x in expression.split(",")]
+
+    ep = prepare_node_structure(arrInp)
+
+    return json.dumps(arrInp)
