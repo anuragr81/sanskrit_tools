c543ab9b11ad24c8ba0d097499c1a52f598d9184
Anurag S <anurag.srivastava@riskcare.com>
2023-06-29 18:01:20 +0100

    Adding condition to chapter 2 sutras.

--- panini/sutras/adhyaaya2/__init__.py
+++ panini/sutras/adhyaaya2/__init__.py
@@ -1,21 +1,15 @@
-from ..common_definitions import Suffix, sup_pratyayaaH,Node
+from ..common_definitions import Suffix, Node
 
-class supodhaatupraatipadikayoH_2040710:
-    def __init__(self):        
-        self._numconditions=1
-    def __call__(self,state):
-        suffixes = [ {'pos':i,'data':st} for i,st in enumerate(state) if isinstance(st,Suffix)]
-        if len(suffixes)>1:
-            # get rid of the second last suffix if it's a sup
-            suffix_string = suffixes[-2]['data']  if isinstance(suffixes[-2]['data'],str) else ''.join(suffixes[-2]['data'].get_suffix())
-            if suffix_string in sup_pratyayaaH():
-                return state[0:suffixes[-2]['pos']] + state[(suffixes[-2]['pos']+1) :]
-        return state
 
 
 class luXtaHprathamasyaXdaaraurasaH_2040850:
     def __init__(self):
         self._numconditions=1
+        self._condition = {'self':{'data':{'domain':["tip","tas","jhi"]},
+                                   'lakaara':{'domain':['luXt']}
+                                   }
+                           
+                           }
     def __call__(self,node):
         if not isinstance(node,Node):
             raise ValueError("node must be of Node type")
